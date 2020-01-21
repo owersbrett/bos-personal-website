cc9c4e0ae0fb8367eb028db53b6333079f9e4c2c
Brett Owers <owersbrett@gmail.com>
2020-01-20 17:55:42 -0700

    got it how i want it. next step is to create blog.

--- core.js
+++ core.js
@@ -1,4 +1,34 @@
 
+
+
+document.getElementById("toggle-menu").addEventListener("click", toggleClasses);
+
+document.querySelectorAll(".nav-elements").forEach(function(element){
+    element.addEventListener("touchstart", toggleColor);
+})
+
+
+
+function toggleColor(e){
+    console.log(e.target);
+    e.target.classList.toggle("element-click")
+    e.target.classList.toggle("element-toggle-div")
+
+}
+
+function toggleClasses(){
+    let menu = document.querySelectorAll(".toggle");
+    let elements = document.querySelectorAll(".nav-elements");
+    console.log(elements[0])
+    elements.forEach(function (element){
+        element.classList.toggle("elements-toggle");
+    });
+    menu[0].classList.toggle("toggle-nav");
+    menu[1].classList.toggle("toggle-menu-container");
+}
+// function dropDownMenu(){
+//     document.getElementById("menu").classList.toggle("show");
+// }
 // // let change = document.querySelector();
 
 // function myFunction() {
